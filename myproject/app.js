310bcf79cbba9d42033da37f3b31c0fb4cd358b1
Leo <rocket.mark10@protonmail.com>
2022-10-14 12:16:40 +0100

    upload final version

---
+++ myproject/app.js
@@ -0,0 +1,20 @@
+/*
+    - What Is Variable ?
+    - Why We Use Variables ?
+    - Declare A Variable And Use
+    - Syntax ( Keyword | Variable Name | Assignment Opeartor | Variable Value )
+    - Variable Without Var
+    - Multiple Variables In The Same Line
+    - Id And Global Variable
+    - Loosely Typed vs Strongly Typed
+*/
+
+var user = "Younes" , age = 21;
+
+console.log("Leo");
+console.log(user);
+console.log(age);
+
+console.log(hello);
+
+hello.innerHTML = "Option";
